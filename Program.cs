904ad9f8cfee478c3a72a69b5c9ba200b21a62c1
zzmxka <52728403+zzmxka@users.noreply.github.com>
2019-12-13 12:15:27 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace program
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            int a = 0;
+            string[,] str1 = { { "人工智能", "3", "zzm", "产生效果" },
+                { "智能化","3","超智能","影响"}
+            };
+
+            foreach (string i in str1)
+            {
+                if (i == str)
+                {
+                    Console.WriteLine("在" + a);
+                }
+                a++;
+                continue;
+            }
+        }
+    }
+}
